85ca840caa9cc60a6776da8833a9fbab384f559e
Tobias Andre Eggers <tobiasae@gmail.com>
2026-03-12 07:23:20 -0300

    adding separator for admin pages

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,10 +1,16 @@
 module Admin
   class BaseController < ApplicationController
     before_action :authenticate_user!
+    before_action :configure_permitted_parameters, if: :devise_controller?
     before_action :require_system_admin!
     around_action :use_public_tenant
 
     private
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :tenant_name])
+    end
+
 
     def require_system_admin!
       return if system_admin?
