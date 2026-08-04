6338b4b4726799aa258c09df144a58b43ed08d95
Tobias Andre Eggers <tobiasae@gmail.com>
2026-08-03 21:17:54 -0300

    updating mortality events page adding new filters

--- app/controllers/mortality_events_controller.rb
+++ app/controllers/mortality_events_controller.rb
@@ -41,11 +41,20 @@ class MortalityEventsController < StockingEventPagesController
   end
 
   def load_active_batch_stockings
+    @units = Unit.order(:name)
+    @selected_unit_id = params[:unit_id].presence
+    @ponds = @selected_unit_id.present? ? Pond.where(unit_id: @selected_unit_id).order(:name) : Pond.order(:name)
+    @selected_pond_id = params[:pond_id].presence
+
     @active_batch_stockings =
       BatchStocking
         .includes(:batch, :pond, :mortality_events)
-        .joins(:batch)
+        .joins(:batch, :pond)
         .where(batches: { status: "active" })
-        .order("batches.name ASC", "batch_stockings.stocked_on DESC")
+
+    @active_batch_stockings = @active_batch_stockings.where(ponds: { unit_id: @selected_unit_id }) if @selected_unit_id.present?
+    @active_batch_stockings = @active_batch_stockings.where(pond_id: @selected_pond_id) if @selected_pond_id.present?
+
+    @active_batch_stockings = @active_batch_stockings.order("batches.name ASC", "batch_stockings.stocked_on DESC")
   end
 end
